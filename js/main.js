e0a7afaa11510f0a6b1d25035424800e7e05efb1
kota_shiratori <swan.kouta@gmail.com>
2024-03-06 15:25:15 +0900

    🚀 todoリスト作成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 const app = Vue.createApp({
     data: () => ({
-        message: 'Hello Vue.js!!'
+        newItem: '',
+        todos: [],
     }),
+    methods: {
+        addItem: function (event) {
+            if(this.newItem === '') return
+            let todo = {
+                item: this.newItem,
+                isDone: false,
+            }
+            this.todos.push(todo)
+            this.newItem = ''
+        },
+        deleteItem: function (index) {
+            this.todos.splice(index, 1)
+        }
+    }
 }).mount('#app')
