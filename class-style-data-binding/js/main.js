53d7d7777791b2943ce36a531263ef79f87b288c
kota_shiratori <swan.kouta@gmail.com>
2024-03-08 23:32:45 +0900

    ♻️ クラスとスタイルのバインディングを学習

--- class-style-data-binding/js/main.js
+++ class-style-data-binding/js/main.js
@@ -1,5 +1,25 @@
 const app = Vue.createApp({
   data: () => ({
-    isLarge: true
+    isLarge: true,
+    hasError: true,
+    largeClass: "large",
+    dangerClass: "text-danger",
+    classObject: {
+      large: true,
+      "text-danger": true,
+    },
+    largeClass: {
+      large: true,
+      "bg-gray": true,
+    },
+    dangerClass: {
+      "text-danger": true,
+    },
+    color: "blue",
+    fontSize: 36,
+    styleObject: {
+      color: "blue",
+      fontSize: "48px",
+    },
   }),
 }).mount("#app");
