cd1e2bc988b514b94b02ca081dddd2f90b94f22d
kota_shiratori <swan.kouta@gmail.com>
2024-03-09 01:40:41 +0900

    🎨 フォーム入力バインディングの学習

---
+++ form-input-binding/js/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data: () => ({
+    message: 'Hello Vue.js!',
+    checked: false,
+    colors: [],
+    color: '',
+    // selected: '',
+    selected: [],
+  }),
+  methods: {
+    
+  }
+}).mount("#app");
